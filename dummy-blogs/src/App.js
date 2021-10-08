e35f22d772a6565efd63c8abe145e0dde671d992
Atul Verma <vermaatul9565@gmail.com>
2021-10-08 15:09:40 +0530

    dynamic-value

--- dummy-blogs/src/App.js
+++ dummy-blogs/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+//import React from 'react';// no need to wirte with 17&+ versoins of react
 function App() {
+  const title = "Welcome to DUMMY BLOGS";
+  const likes = 1000;
+  const link = "https://www.google.com";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="component">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>Random Generator: { Math.random() * 10 }</p>
+        <p><a href={ link }>google.com</a></p>
+      </div>
     </div>
   );
 }
