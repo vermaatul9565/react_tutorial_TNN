cb8da0d63f5e8edb0ae722bca5f03d98d5f308b4
Atul Verma <vermaatul9565@gmail.com>
2021-10-09 12:40:17 +0530

    components

--- dummy-blogs/src/App.js
+++ dummy-blogs/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 //import React from 'react';// no need to wirte with 17&+ versoins of react
+
+import Navbar from './Navbar';
+import Home from './Home';
 function App() {
-  const title = "Welcome to DUMMY BLOGS";
-  const likes = 1000;
-  const link = "https://www.google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="component">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>Random Generator: { Math.random() * 10 }</p>
-        <p><a href={ link }>google.com</a></p>
+        <Home />
       </div>
     </div>
   );
---
+++ dummy-blogs/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Dummy Blogs</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
