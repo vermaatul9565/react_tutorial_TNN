34d04e30dd5959238eadeb252c3902ba8287d2c1
Atul Verma <vermaatul9565@gmail.com>
2021-10-13 12:40:52 +0530

    click event

--- dummy-blogs/src/Home.js
+++ dummy-blogs/src/Home.js
@@ -1,6 +1,16 @@
 const Home = () => {
+    const handleClick = () =>{
+        console.log('hello, ATUL !!');
+    }
+    const handleClickAgain = (name) =>{
+        console.log('hello', name);
+    }
     return ( 
-        <h2>Home Page</h2>
+        <div className="home">
+            <h2>Home Page</h2>
+            <button onClick={handleClick}>click</button>
+            <button onClick={ ()=>{ handleClickAgain("Mario !!")} }>click again</button>
+        </div>
      );
 }
  
